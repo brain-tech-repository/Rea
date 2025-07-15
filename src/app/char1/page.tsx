9112f43741ebc6194ed589d0a0df423c57b9c0cd
rajneeshkumar1111 <rajneeshmkumar1111@gmail.com>
2025-07-15 18:05:47 +0530

    Add Page

---
+++ src/app/char1/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Charpage1 from '@/Components/Characters/Charpage1'
+const page = () => {
+  return (
+    <>
+    <Charpage1/>
+    </>
+  )
+}
+
+export default page
